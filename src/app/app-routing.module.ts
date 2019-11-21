2394c672d99823b0e819ece41c938c5c99e4b670
lyus1216 <lyus@pchome.com.tw>
2019-11-21 14:39:53 +0800

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
+import { IntroductionComponent } from "./introduction/introduction.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { ContentComponent } from "./content/content.component";
+import { TestimonialsComponent } from "./testimonials/testimonials.component";
+import { ClientsComponent } from "./clients/clients.component";
+import { PricingComponent } from "./pricing/pricing.component";
+import { HeaderComponent } from "./header/header.component";
 
+const routes: Routes = [
+  { path: "", redirectTo: "/Home", pathMatch: "full" },
+  { path: "Home", component: HeaderComponent },
+  { path: "About", component: IntroductionComponent },
+  { path: "Gallery", component: GalleryComponent },
+  { path: "Services", component: ContentComponent },
+  { path: "Testimonials", component: TestimonialsComponent },
+  { path: "Clients", component: ClientsComponent },
+  { path: "Pricing", component: PricingComponent }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
