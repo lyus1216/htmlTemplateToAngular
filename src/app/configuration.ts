521108fe3996813c3790a5635dbcb000d701ddf4
lyus1216 <lyus@pchome.com.tw>
2019-11-22 16:27:21 +0800

    Add configuration

---
+++ src/app/configuration.ts
@@ -0,0 +1,15 @@
+
+
+export const Configuration {
+    
+    header = {
+        heading: "My website",
+        headingtext:
+        "Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects, enjoy!",
+        buttontext: "START CREATING TODAY",
+        buttonlink: "/Home"
+      },
+    
+}
+
+
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,12 +6,6 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-  header = {
-    heading: "My website",
-    headingtext:
-      "Namari is a free landing page template you can use for your projects. It is free to use for your personal and commercial projects, enjoy!",
-    buttontext: "START CREATING TODAY"
-  };
   constructor() {}
 
   ngOnInit() {}
